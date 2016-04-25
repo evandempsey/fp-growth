e13d8db8bdacf55d621c1fe793fdd0ae3b35afc9
Evan Dempsey <evandempsey@gmail.com>
2016-04-25 17:35:15 +0200

    Added __version__ to __init__.py.

---
+++ fpgrowth/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .fpgrowth import FPTree, generate_association_rules  # flake8: noqa
+
+__author__ = 'Evan Dempsey'
+__email__ = 'me@evandempsey.io'
+__version__ = '1.0'
